9b25b49a69bc565a29aa830e8b78e56f8f288e3c
Philip Chang <sgnoohc@gmail.com>
2017-04-03 16:19:29 -0500

    cleaned up scripts code

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -7,6 +7,7 @@
 
 # quick plotting script from the histogram root files
 
+# if the third option is provided you can draw a shape comparison only plot
 if [ "x$3" == "x" ]; then
   SIGSCALE=5
   SCALE=36
@@ -20,6 +21,9 @@ else
   BKGCOLORING="Line"
   LINEWIDTH=2
 fi
+
+# The plot will be created here http://uaf-7.t2.ucsd.edu/~phchang/test.pdf or http://uaf-7.t2.ucsd.edu/~${USERNAME}/test.pdf
+mkdir -p public_html
 
 python scripts/makeplot.py \
   --plottype plot1d \
