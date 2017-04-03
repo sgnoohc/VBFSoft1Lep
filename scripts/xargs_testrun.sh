a49b5d5a2fe51a75c7a9e20fa65c5e759bfaf5f8
Philip Chang <sgnoohc@gmail.com>
2017-04-03 16:03:22 -0500

    removed test scripts

--- scripts/xargs_testrun.sh
+++
@@ -1,8 +0,0 @@
-cores=38
-mkdir -p output
-sh scripts/link_input_files.sh
-xargs --arg-file=scripts/testjobs.txt \
-      --max-procs=$cores  \
-      --replace \
-      --verbose \
-      /bin/sh -c "{}"
