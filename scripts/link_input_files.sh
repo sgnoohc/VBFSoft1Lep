b65da8e87df2370ace352887e54090ce016b4409
Philip Chang <sgnoohc@gmail.com>
2017-04-03 16:11:32 -0500

    moved input to input/

--- scripts/link_input_files.sh
+++ scripts/link_input_files.sh
@@ -1,2 +1,5 @@
+mkdir -p input
+cd input
 ln -sf /home/users/phchang/analyses/vbfsusysoft1lep/babymaker-MT2Analysis-soft/MT2Analysis/babymaker/unknown_test.root signal.root
 ln -sf /nfs-6/userdata/mt2/softLep-v3_V00-01-10_25ns_miniaodv2
+cd -
