bd6a9419809dc4dfb61a9f3eeeb92f91dd8c7fa7
Philip Chang <sgnoohc@gmail.com>
2017-04-28 00:11:50 -0700

    msg

---
+++ src/MT2TreeConnector.cxx
@@ -0,0 +1,32 @@
+// vim: tabstop=2:softtabstop=2:shiftwidth=2:expandtab
+
+//  .
+// ..: P. Chang, philip@physics.ucsd.edu
+
+#include "MT2TreeConnector.h"
+
+ObjUtil::Leptons getLeptons(MT2Tree& mytree)
+{
+  ObjUtil::Leptons leptons;
+  return leptons;
+}
+
+ObjUtil::Jets getJets(MT2Tree& mytree)
+{
+  ObjUtil::Jets jets;
+  return jets;
+}
+
+ObjUtil::METs getMETs(MT2Tree& mytree)
+{
+  ObjUtil::METs mets;
+  return mets;
+}
+
+ObjUtil::MET getMET(MT2Tree& mytree)
+{
+  ObjUtil::MET met;
+  return met;
+}
+
+//eof
---
+++ src/MT2TreeConnector.h
@@ -0,0 +1,20 @@
+// vim: tabstop=2:softtabstop=2:shiftwidth=2:expandtab
+
+//  .
+// ..: P. Chang, philip@physics.ucsd.edu
+
+#ifndef MT2TreeConnector_h
+#define MT2TreeConnector_h
+
+#include "Ditto.h"
+#include "MT2Tree.h"
+
+using namespace Ditto;
+
+ObjUtil::Leptons getLeptons(MT2Tree& mytree);
+ObjUtil::Jets getJets(MT2Tree& mytree);
+ObjUtil::METs getMETs(MT2Tree& mytree);
+ObjUtil::MET getMET(MT2Tree& mytree);
+
+#endif
+//eof
