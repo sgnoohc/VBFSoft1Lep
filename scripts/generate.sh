7981cf173b7f5d4a663ebc8159374a34a5219d66
Philip Chang <sgnoohc@gmail.com>
2017-05-04 17:04:19 -0700

    msg

--- scripts/generate.sh
+++
@@ -1,5 +0,0 @@
-python scripts/make_classfiles.py -t mt2 input/signal.root  -l -c MT2Tree -o mt2tree_obj -n mt2tree
-yes | mv MT2Tree.cc src/MT2Tree.cxx
-yes | mv MT2Tree.h src/MT2Tree.h
-rm ScanChain.C
-rm doAll.C
--- scripts/print_nonzero_mass_point.C
+++
@@ -1,6 +0,0 @@
-void print_nonzero_mass_point()
-{
-  TFile* file = new TFile("haddoutput/hist_signal.root");
-  TH1F* hist = (TH1F*) file->Get("rawcutflow_0.0_150.7_132.4");
-  hist->Print("all");
-}
